259c9f578b12eb14315b7559af38d2006f7fa7c6
AbrRahman <abrahman.est@gmail.com>
2022-10-27 17:08:01 +0600

    adde course route

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
-
+const cors = require('cors');
+const courseName = require('./data/courseName')
+const course = require('./data/course')
+// const news = require('./data/news')
+app.use(cors())
 dotenv.config();
 
 app.get('/', (req, res) => {
     res.status(200).send('This is home route')
 })
-
-
-
-
+// course name
+app.get('/course-name', (req, res) => {
+    res.send(courseName);
+})
+//  get all course
+app.get('/course', (req, res) => {
+    res.send(course);
+})
+// get singal course
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    const data = course.find(c => c._id === id)
+    res.send(data)
+})
 
 
 
